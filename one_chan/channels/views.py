4ff0106d45129a964a8c8121ae13d825f8cc038f
Mikerah <mikerah14@gmail.com>
2017-03-03 18:35:21 -0500

    Added new thread functionality

--- one_chan/channels/views.py
+++ one_chan/channels/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
 from .models import *
+from .forms import *
 
 def index(request):
     list_of_boards = Board.objects.all()
@@ -7,5 +9,14 @@ def index(request):
     
 def board(request, board_name):
     board = get_object_or_404(Board, board_name=board_name)
-    context = {'board': board}
+    if request.method == 'POST':
+        new_thread_form = ThreadForm(request.POST)
+        if new_thread_form.is_valid():
+            new_thread = Thread(board=board, thread_title=new_thread_form.cleaned_data['thread_title'], \
+                            author=new_thread_form.cleaned_data['author'], thread_post=new_thread_form.cleaned_data['thread_post'])
+            new_thread.save()
+            return HttpResponseRedirect('/{}'.format(board))
+    else:
+        new_thread_form = ThreadForm()
+    context = {'board': board, 'thread_form': new_thread_form}
     return render(request, 'channels/board.html', context)
