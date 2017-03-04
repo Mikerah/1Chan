697c9c9c99dbace326f7bdae383a01869b37739d
Mikerah <mikerah14@gmail.com>
2017-03-03 19:16:24 -0500

    Added part of the reply form functionality

--- one_chan/channels/views.py
+++ one_chan/channels/views.py
@@ -10,13 +10,19 @@ def index(request):
 def board(request, board_name):
     board = get_object_or_404(Board, board_name=board_name)
     if request.method == 'POST':
-        new_thread_form = ThreadForm(request.POST)
-        if new_thread_form.is_valid():
-            new_thread = Thread(board=board, thread_title=new_thread_form.cleaned_data['thread_title'], \
-                            author=new_thread_form.cleaned_data['author'], thread_post=new_thread_form.cleaned_data['thread_post'])
-            new_thread.save()
-            return HttpResponseRedirect('/{}'.format(board))
+        if 'new_thread' in request.POST:
+            new_thread_form = ThreadForm(request.POST)
+            if new_thread_form.is_valid():
+                new_thread = Thread(board=board, thread_title=new_thread_form.cleaned_data['thread_title'], \
+                                author=new_thread_form.cleaned_data['author'], thread_post=new_thread_form.cleaned_data['thread_post'])
+                new_thread.save()
+                return HttpResponseRedirect('/{}'.format(board))
+        elif 'new_reply' in request.POST:
+            new_reply_form = ReplyForm(request.POST)
+            if new_reply_form.is_valid():
+                new_reply = Reply()
     else:
         new_thread_form = ThreadForm()
-    context = {'board': board, 'thread_form': new_thread_form}
+        new_reply_form = ReplyForm()
+    context = {'board': board, 'thread_form': new_thread_form, 'reply_form': new_reply_form}
     return render(request, 'channels/board.html', context)
