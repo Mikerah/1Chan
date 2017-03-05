3ee1f98824fd37f5c6b12172f99f9a4434f15160
Mikerah <mikerah14@gmail.com>
2017-03-05 07:55:09 -0500

    Completed most of the reply functionality. Simply need to change the UI parts

--- one_chan/channels/views.py
+++ one_chan/channels/views.py
@@ -13,14 +13,21 @@ def board(request, board_name):
         if 'new_thread' in request.POST:
             new_thread_form = ThreadForm(request.POST)
             if new_thread_form.is_valid():
-                new_thread = Thread(board=board, thread_title=new_thread_form.cleaned_data['thread_title'], \
-                                author=new_thread_form.cleaned_data['author'], thread_post=new_thread_form.cleaned_data['thread_post'])
+                thread_title = new_thread_form.cleaned_data['thread_title']
+                author = new_thread_form.cleaned_data['author']
+                thread_post = new_thread_form.cleaned_data['thread_post']
+                new_thread = Thread(board=board, thread_title= thread_title, author=author, thread_post= thread_post)
                 new_thread.save()
                 return HttpResponseRedirect('/{}'.format(board))
         elif 'new_reply' in request.POST:
             new_reply_form = ReplyForm(request.POST)
             if new_reply_form.is_valid():
-                new_reply = Reply()
+                thread = Thread.objects.get(id=request.POST['thread_id'])
+                author = new_reply_form.cleaned_data['author']
+                reply = new_reply_form.cleaned_data['reply']
+                new_reply = Reply(thread=thread, author=author, reply=reply)
+                new_reply.save()
+                return HttpResponseRedirect('/{}'.format(board))
     else:
         new_thread_form = ThreadForm()
         new_reply_form = ReplyForm()
